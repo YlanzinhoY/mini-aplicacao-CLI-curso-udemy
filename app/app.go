2478d5f4f12ceda34bdc4f25db24566491ae19f9
Enzo <Enzo.maxpayne@gmail.com>
2023-05-16 19:42:11 -0300

    finalizando o projeto

--- app/app.go
+++ app/app.go
@@ -1,23 +1,70 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+	"github.com/urfave/cli"
+)
+
 
 func Gerar() *cli.App {
+
+	flags := []cli.Flag {
+		cli.StringFlag{
+			Name: "host",
+			Value: "localhost",
+		},
+	}
 
 	app := cli.NewApp()
 	app.Name = "Aplicação de linha Comando"
 	app.Usage = "Busca IPs e Nome de servidor na internet"
 
 	app.Commands = []cli.Command {
-		Name: "ip",
-		Usage: "Buscas IPS de endereços na internet",
-		Flags: []cli.Flag {
-			cli.StringFlag{
-				Name: "host",
-				Value:  "github.com/YlanzinhoY" ,
-			}
-		}
+		{
+			Name: "ip",
+			Usage: "Busca IPS de endereços na internet",
+			Flags: flags,
+			Action: buscarIps ,
+		},
+		{
+			Name: "servidores",
+			Usage: "busca o nome dos servidores na internet",
+			Flags: flags,
+			Action: buscarServidores,
+		},
 	}
-
 	return app
 }
+
+func buscarIps(c *cli.Context) {
+	host := c.String("host")
+
+	ips, erro := net.LookupIP(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
+}
+
+
+
+func buscarServidores(c *cli.Context) {
+	host := c.String("host")
+
+	servidor, erro := net.LookupNS(host)
+	if erro != nil {
+		log.Fatal(erro)
+	}
+
+	for _, servidor := range servidor {
+		fmt.Println(servidor.Host)
+	}
+}
+
+
+
